aad9254976933fb459adcfd2f8d49ee509b1f415
Marcos Souza <marcos.souza@beeleads.com.br>
2025-05-16 09:20:36 -0300

    Permissões por cargo

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,19 @@
+import { AbilityBuilder } from '@casl/ability'
+import { AppAbility } from '.'
+import { User } from './models/user'
+
+type Roles = 'ADMIN' | 'MEMBER'
+
+type PermissionsByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>
+) => void
+
+export const permissions: Record<Roles, PermissionsByRole> = {
+  ADMIN(_, { can }) {
+    can('manage', 'all')
+  },
+  MEMBER(_, { can }) {
+    can('invite', 'User')
+  },
+}
